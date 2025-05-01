d57672c7a9a9c29957216eaea9b877347a5d3a3b
ozanardine <ozanardine@gmail.com>
2025-04-30 22:11:33 -0300

    Ajustes 1.3

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -306,8 +306,14 @@ const Products: React.FC = () => {
 
       {/* Modal do Produto */}
       {selectedProduct && (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
-          <div className="bg-white rounded-lg max-w-4xl w-full max-h-[90vh] overflow-y-auto">
+        <div 
+          className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4"
+          onClick={() => setSelectedProduct(null)}
+        >
+          <div 
+            className="bg-white rounded-lg max-w-4xl w-full max-h-[90vh] overflow-y-auto"
+            onClick={(e) => e.stopPropagation()}
+          >
             <div className="relative">
               <button
                 onClick={() => setSelectedProduct(null)}
